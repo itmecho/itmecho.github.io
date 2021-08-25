5882db66b8927022d21a800150b85376ef9fed69
Iain Earl <iain@itmecho.com>
2021-08-25 16:32:35 +0100

    Initial mantine homepage

---
+++ pages/_document.tsx
@@ -0,0 +1,58 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { SheetsRegistry, JssProvider, createGenerateId } from "react-jss";
+
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const registry = new SheetsRegistry();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          (
+            <JssProvider registry={registry} generateId={createGenerateId()}>
+              <App {...props} />
+            </JssProvider>
+          ),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style id="mantine-ssr-styles">{registry.toString()}</style>
+        </>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Expletus+Sans&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
