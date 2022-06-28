1fbbd7a8df06e6653e5673c3152b3b751c19b8a6
Iain Earl <iain@itmecho.com>
2022-06-28 13:43:06 +0100

    Fully functional first post!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          'Libre Franklin',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Segoe UI',
-          'Roboto',
-          'Helvetica Neue',
-          'Arial',
-          'Noto Sans',
-          'sans-serif',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol',
-          'Noto Color Emoji',
-        ],
+        sans: ['Libre Franklin', ...defaultTheme.fontFamily.sans],
+        mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
         brand: ['Expletus Sans'],
       },
     },
