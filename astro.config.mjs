ba02a2167a4d1d6907a5e1021e322be29d4b411d
Iain Earl <iain@itmecho.com>
2022-11-09 22:58:41 +0000

    Upgrade to astro 1.6.5
    Switches posts to use MDX and other related changes for the v1 release

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+
+import { remarkReadingTime } from './plugins/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkReadingTime],
+    }),
+    tailwind(),
+  ],
 });
