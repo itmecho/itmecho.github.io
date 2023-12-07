9d8e91bed8d79fa53a1b1e0b61ddcf723247acd2
Iain Earl <iain@itmecho.com>
2023-12-07 16:43:04 +0000

    Formatting

--- src/lib/cx.ts
+++ src/lib/cx.ts
@@ -1,5 +1,6 @@
 type ClassOption = string | boolean | undefined;
 
-export const cx = (...classes: ClassOption[]) => classes.filter((c) => !!c).join(' ');
+export const cx = (...classes: ClassOption[]) =>
+  classes.filter((c) => !!c).join(' ');
 
 export default cx;
